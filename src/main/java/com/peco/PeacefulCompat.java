b6f048dcf91e70d0ac719a4c1a269cce248f6738
omerbenda <omerbenda@gmail.com>
2023-08-16 17:14:34 +0300

    fix(Config.java): Removal of unused class

--- src/main/java/com/peco/PeacefulCompat.java
+++ src/main/java/com/peco/PeacefulCompat.java
@@ -11,9 +11,7 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -37,8 +35,6 @@ public class PeacefulCompat {
     MinecraftForge.EVENT_BUS.register(new LootEventListener());
 
     modEventBus.addListener(this::addCreative);
-
-    ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
   }
 
   private void commonSetup(final FMLCommonSetupEvent event) {}
